d1843a080453310af16148d345832ea7107cd0de
sublun <282734365@qq.com>
2019-07-12 18:36:05 +0800

    111

--- gitupload/src/com/itheima/git/FirstClass.java
+++ gitupload/src/com/itheima/git/FirstClass.java
@@ -26,29 +26,4 @@ public class FirstClass {
         System.out.println("2222222");
     }
 }
-package com.itheima.git;
 
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-    }
-    void id(){
-        int id=0;
-        id +=1;
-        id +=1;
-        id +=1;
-        id +=1;
-        id +=1;
-        id +=1;
-        System.out.println("1111111");
-        System.out.println("1111111");
-        System.out.println("1111111");
-        System.out.println("3333333");
-        System.out.println("3333333");
-        System.out.println("3333333");
-        System.out.println("3333333");
-        System.out.println("3333333");
-    }
-}
